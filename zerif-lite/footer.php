50369384b39e3ca74fcf5fc9d893f6effd0aa6b3
Daniel Cook <dscook@Daniels-MacBook-Air.local>
2016-02-16 09:52:44 +0000

    Added theme.

---
+++ zerif-lite/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer.
+ * Contains the closing of the #content div and all content after
+ */
+
+?>
+
+</div><!-- .site-content -->
+
+	</div><!-- mobile-bg-fix-whole-site -->
+</div><!-- .mobile-bg-fix-wrap -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
